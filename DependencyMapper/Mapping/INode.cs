95c0d4b513bfceb89af914e3eccc6ff249c2ae7d
GraemeB <grae22@gmail.com>
2020-07-24 08:33:05 +0200

    Add Node class.

---
+++ DependencyMapper/Mapping/INode.cs
@@ -0,0 +1,13 @@
+﻿namespace DependencyMapper.Mapping
+{
+  public interface INode
+  {
+    string Name { get; }
+    string Description { get; }
+    string Category { get; }
+
+    void UpdateName(in string name);
+    void UpdateDescription(in string description);
+    void UpdateCategory(in string category);
+  }
+}
---
+++ DependencyMapper/Mapping/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DependencyMapper.Mapping
+{
+  internal class Node : INode
+  {
+    public int Id { get; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public string Category { get; private set; }
+
+    public Node(
+      in int id,
+      in string name,
+      in string description,
+      in string category)
+    {
+      if (id < 1)
+      {
+        throw new ArgumentException($"{nameof(id)} cannot be zero or negative.");
+      }
+
+      Id = id;
+      Name = name;
+      Description = description;
+      Category = category;
+    }
+
+    public void UpdateName(in string name)
+    {
+      Name = name;
+    }
+
+    public void UpdateDescription(in string description)
+    {
+      Description = description;
+    }
+
+    public void UpdateCategory(in string category)
+    {
+      Category = category;
+    }
+  }
+}
